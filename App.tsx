fd7c4147bcba5a5bfe460574ce2399fde02bf5d2
Ryu <96819668+balikis-oyeleye@users.noreply.github.com>
2024-03-02 10:34:01 +0100

    setup

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import React from 'react';
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>
+        App Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sit
+        expedita porro aperiam fuga doloremque est dolorum. Maxime ratione
+        minima dolorum necessitatibus cupiditate, quia ut mollitia fugiat nisi
+        autem. Et?
+      </Text>
     </View>
   );
 };
